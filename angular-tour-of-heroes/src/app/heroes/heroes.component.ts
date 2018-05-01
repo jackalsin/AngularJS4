de8f9de4dc8651677bd25f9e85be0e2ade35536d
zhiwei.xin <zhiwei.xin@veritas.com>
2018-05-01 16:06:29 -0700

    Add Hero

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -7,9 +7,9 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero : Hero = {
-    id : 1,
-    name : "Windstorm"
+  hero: Hero = {
+    id: 1,
+    name: "Windstorm"
   }
   constructor() { }
 
